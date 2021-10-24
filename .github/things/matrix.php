823d9e3d84fd6dae18020cd2d4a1444e616c7ba7
Steve Boyd <emteknetnz@gmail.com>
2021-10-24 20:02:02 +1300

    Update matrix.php

--- .github/things/matrix.php
+++ .github/things/matrix.php
@@ -31,16 +31,37 @@ foreach ($inputs as $input => $value) {
 }
 
 $matrix = ['include' => []];
-foreach ($defaultJobs as $arr) {
-    foreach (array_keys($arr) as $test) {
-        if ($test === 'php' || !isset($runTests[$test]) || !$runTests[$test]) {
-            continue;
-        }
-        $matrix['include'][] = $arr;
-    }
+
+// foreach ($defaultJobs as $arr) {
+//     foreach (array_keys($arr) as $test) {
+//         if ($test === 'php' || !isset($runTests[$test]) || !$runTests[$test]) {
+//             continue;
+//         }
+//         $matrix['include'][] = $arr;
+//     }
+// }
+
+if (file_exists('phpunit.xml') || file_exists('phpunit.xml.dist')) {
+    $matrix['include'][] = ['php' => '7.3', 'phpunit' => true, 'composer_arg' => '--prefer-lowest'];
+    $matrix['include'][] = ['php' => '7.4', 'phpunit' => true];
 }
+if (file_exists('phpcs.xml') || file_exists('phpcs.xml.dist')) {
+    $matrix['include'][] = ['php' => '7.4', 'phplinting' => true];
+}
+// TODO: no codecov file in silverstripe-admin, so cannot feature detect. should probably run by default and allow disabling with 'no_phpcoverage'?
+if (file_exists('behat.yml')) {
+    $matrix['include'][] = ['php' => '7.3', 'endtoend' => true];
+}
+if (file_exists('package.json')) {
+    $matrix['include'][] = ['php' => '7.4', 'js' => true];
+}
+
 foreach ($extraJobs as $arr) {
     $matrix['include'][] = $arr;
+}
+
+foreach ($matrix['include'] as $arr) {
+    $arr['composer_arg] ??= ''; 
 }
 
 $json = json_encode($matrix);
