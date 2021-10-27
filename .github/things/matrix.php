59f8b56a858be6ee9bef7b38af54f492b1fdac57
Steve Boyd <emteknetnz@gmail.com>
2021-10-27 16:21:18 +1300

    Split phpunit suites into different jobs

--- .github/things/matrix.php
+++ .github/things/matrix.php
@@ -42,9 +42,23 @@ $matrix = ['include' => []];
 // }
 
 if (file_exists('phpunit.xml') || file_exists('phpunit.xml.dist')) {
-    $matrix['include'][] = ['php' => '7.3', 'phpunit' => true, 'composer_arg' => '--prefer-lowest'];
-    $matrix['include'][] = ['php' => '7.4', 'phpunit' => true];
+    $fn = file_exists('phpunit.xml') ? 'phpunit.xml' : 'phpunit.xml.dist';
+    $d = new DOMDocument();
+    $d->preserveWhiteSpace = false;
+    $d->load($fn);
+    $x = new DOMXPath($d);
+    $tss = $x->query('//testsuite');
+    foreach ($tss as $ts) {
+        if (!$ts->hasAttribute('name') || $ts->getAttribute('name') == 'Default') {
+            continue;
+        }
+        $matrix['include'][] = ['php' => '7.4', 'phpunit' => true, 'phpunit_suite' => $ts->getAttribute('name')];
+    }
+    if (count($matrix) == 0) {
+        $matrix['include'][] = ['php' => '7.4', 'phpunit' => true, 'phpunit_suite' => ''];
+    }
 }
+
 if (file_exists('phpcs.xml') || file_exists('phpcs.xml.dist')) {
     $matrix['include'][] = ['php' => '7.4', 'phplinting' => true];
 }
