a3799b1f543364769ed6dcf4a107d8f101ac2e00
Steve Boyd <emteknetnz@gmail.com>
2021-11-04 09:40:56 +1300

    .

--- .github/things/matrix.php
+++ .github/things/matrix.php
@@ -3,7 +3,7 @@
 // Reads inputs.yml and creates a new json matrix
 
 $inputs = yaml_parse(file_get_contents('inputs.yml'));
-$runTests = [];
+$run = [];
 $defaultJobs = [];
 $extraJobs = [];
 foreach ($inputs as $input => $value) {
@@ -15,8 +15,8 @@ foreach ($inputs as $input => $value) {
             $value = false;
         }
         // e.g. run_phplinting => phplinting
-        $test = str_replace('run_', '', $input);
-        $runTests[$test] = $value;
+        $type = str_replace('run_', '', $input);
+        $run[$type] = $value;
     } else if ($input === 'default_jobs') {
         if ($value === 'none') {
             $value = [];
@@ -32,7 +32,7 @@ foreach ($inputs as $input => $value) {
 
 $matrix = ['include' => []];
 
-if (file_exists('phpunit.xml') || file_exists('phpunit.xml.dist')) {
+if ((file_exists('phpunit.xml') || file_exists('phpunit.xml.dist')) && $run['phpunit']) {
     $fn = file_exists('phpunit.xml') ? 'phpunit.xml' : 'phpunit.xml.dist';
     $d = new DOMDocument();
     $d->preserveWhiteSpace = false;
@@ -43,22 +43,27 @@ if (file_exists('phpunit.xml') || file_exists('phpunit.xml.dist')) {
         if (!$ts->hasAttribute('name') || $ts->getAttribute('name') == 'Default') {
             continue;
         }
-        $matrix['include'][] = ['php' => '7.4', 'phpunit' => true, 'phpunit_suite' => $ts->getAttribute('name')];$matrix['include'][] = ['php' => '8.0', 'phpunit' => true, 'phpunit_suite' => $ts->getAttribute('name')];
+        $matrix['include'][] = ['php' => '7.4', 'phpunit' => true, 'phpunit_suite' => $ts->getAttribute('name')];
+        if ($run['phpunit_php8']) {
+            $matrix['include'][] = ['php' => '8.0', 'phpunit' => true, 'phpunit_suite' => $ts->getAttribute('name')];
+        }
     }
-    if (count($matrix) == 0) {
+    if (count($matrix) == 0 && $run['phpunit']) {
         $matrix['include'][] = ['php' => '7.4', 'phpunit' => true, 'phpunit_suite' => ''];
-        $matrix['include'][] = ['php' => '8.0', 'phpunit' => true, 'phpunit_suite' => ''];
+        if ($run['phpunit_php8']) {
+            $matrix['include'][] = ['php' => '8.0', 'phpunit' => true, 'phpunit_suite' => ''];
+        }
     }
 }
 
-if (file_exists('phpcs.xml') || file_exists('phpcs.xml.dist')) {
+if ((file_exists('phpcs.xml') || file_exists('phpcs.xml.dist')) && $run['phpcoverage']) {
     $matrix['include'][] = ['php' => '7.4', 'phplinting' => true];
 }
 // TODO: no codecov file in silverstripe-admin, so cannot feature detect. should probably run by default and allow disabling with 'no_phpcoverage'?
-if (file_exists('behat.yml')) {
+if (file_exists('behat.yml') && $run['endtoend']) {
     $matrix['include'][] = ['php' => '7.3', 'endtoend' => true];
 }
-if (file_exists('package.json')) {
+if (file_exists('package.json') && $run['js']) {
     $matrix['include'][] = ['php' => '7.4', 'js' => true];
 }
 
